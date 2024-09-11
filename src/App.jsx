f58a0b27adf32167e33b4f54531f8fa6ab8cc395
Cova21 <153543362+Cova21@users.noreply.github.com>
2024-09-11 13:10:27 +0300

    change 01

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+isAdult = true
+  return (
+<>
+  <snap>(isAdult ? "da" : "net")</snap>
+</>
+  )
+}
+
+export default App
